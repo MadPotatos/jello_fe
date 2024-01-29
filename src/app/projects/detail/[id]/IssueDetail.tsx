f267009f8b8bd70dd08025520e093725eb1cdb35
viet <viet7896@gmail.com>
2024-01-29 16:27:10 +0700

    FEATURE: Create comment and delete

--- src/app/projects/detail/[id]/IssueDetail.tsx
+++ src/app/projects/detail/[id]/IssueDetail.tsx
@@ -1,12 +1,15 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { Button, Col, Form, Input, Modal, Row, Select, Space, List } from 'antd';
+import { Button, Col, Form, Input, Modal, Row, Select, Space, List, message } from 'antd';
 import { Avatar, Typography } from 'antd';
 import { Comment } from '@ant-design/compatible';
 import { Backend_URL } from '@/lib/Constants';
 import { usePathname } from 'next/navigation';
 import { Member } from '@/lib/types';
 import { getColoredIconByIssueType, getColoredIconByPriority } from '@/lib/utils';
+import { useSession } from 'next-auth/react';
+import { Popconfirm } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
 
 interface IssueDetailModalProps {
   issue: any;
@@ -17,7 +20,9 @@ interface IssueDetailModalProps {
 
 const IssueDetailModal: React.FC<IssueDetailModalProps> = ({ issue, lists,visible, onClose }) => {
   const [members, setMembers] = useState<Member[]>([]);
+  const [comments, setComments] = useState<any[]>([]);
   const pathname = usePathname();
+  const { data: session } = useSession();
   const projectId = Number(pathname.split('/')[3]);
   const [form] = Form.useForm();
 
@@ -32,6 +37,17 @@ const IssueDetailModal: React.FC<IssueDetailModalProps> = ({ issue, lists,visibl
         }
     };
 
+    const fetchComments = async (id: number) => {
+        try {
+        const response = await fetch(`${Backend_URL}/comments/${id}`);
+        const data = await response.json();
+        return data;
+        } catch (error) {
+        console.error('Error fetching comments:', error);
+        throw error;
+        }
+    }
+
 const typeOptions = [
   { label: <span>{getColoredIconByIssueType(1)} Task</span>, value: 1 },
   { label: <span>{getColoredIconByIssueType(2)} Bug</span>, value: 2 },
@@ -44,8 +60,6 @@ const priorityOptions = [
   { label: <span>{getColoredIconByPriority(3)} Low</span>, value: 3 }
 ];
 
-
-  const comments = issue.comments || [];
   const reporter = members.find(member => member.userId === issue.reporterId);
   const defaultAssigneeIds = issue.assignees.map((assignee: { userId: string }) => Number(assignee.userId));
 
@@ -53,7 +67,9 @@ const priorityOptions = [
     const fetchData = async () => {
       try {
         const membersData = await fetchMembers(projectId);
+        const commentsData = await fetchComments(issue.id);
         setMembers(membersData);
+        setComments(commentsData);
       if (issue && members) {
       form.setFieldsValue({
       type: issue.type,
@@ -70,6 +86,57 @@ const priorityOptions = [
     fetchData();
   }
   , [projectId,issue]);
+
+  const addComment = async (commentText: string) => {
+  try {
+    const response = await fetch(`${Backend_URL}/comments`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session?.backendTokens.accessToken}`,
+      },
+      body: JSON.stringify({
+        userId: session?.user.id,
+        issueId: issue.id,
+        descr: commentText,
+      }),
+    });
+
+    if (response.ok) {
+      message.success('Comment added successfully');
+      const data = await response.json();
+      setComments([...comments, data]);
+    } else {
+      // Handle error case
+      console.error('Failed to add comment');
+    }
+  } catch (error) {
+    console.error('Error adding comment:', error);
+  }
+};
+
+const handleDeleteComment = async (commentId: number) => {
+  try {
+    const response = await fetch(`${Backend_URL}/comments/${commentId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${session?.backendTokens.accessToken}`,
+      },
+    });
+
+    if (response.ok) {
+      message.success('Comment deleted successfully');
+      setComments(comments.filter(comment => comment.id !== commentId));
+    } else {
+      // Handle error case
+      console.error('Failed to delete comment');
+    }
+  } catch (error) {
+    console.error('Error deleting comment:', error);
+  }
+};
+
+
 
   return (
     console.log(form.getFieldsValue()),
@@ -90,34 +157,47 @@ const priorityOptions = [
     >
       <Row>
         <Col span={16}>
+             
           <div className="p-4">
-            <Typography.Title level={4}>{issue.name}</Typography.Title>
             <Typography.Paragraph>{issue.descr}</Typography.Paragraph>
             <List
-              className="comment-list"
+              className="comment-list max-h-[450px] overflow-y-auto"
               header={`${comments.length} comments`}
               itemLayout="horizontal"
               dataSource={comments}
               renderItem={(comment: any) => (
                 <li>
-                  <Comment
-                    author={comment.author}
-                    avatar={<Avatar src={comment.avatar} alt={comment.author} />}
-                    content={comment.content}
-                    datetime={new Date(comment.datetime).toLocaleString()}
-                  />
+                    <div className='flex justify-between items-center'>
+                    <div>
+                      <Comment
+                        author={<Typography.Text style={{ fontSize: '16px' }}>{comment.name}</Typography.Text>}
+                        avatar={<Avatar src={comment.avatar} alt={comment.name} />}
+                        content={comment.descr}
+                        datetime={new Date(comment.createdAt).toLocaleString()}
+                      />
+                    </div>
+                    {session?.user.id === comment.userId && (
+                       <Popconfirm
+                          title="Are you sure you want to delete this comment?"
+                          onConfirm={() => handleDeleteComment(comment.id)}
+                          okText="Yes"
+                          okButtonProps={{style: {backgroundColor: '#1890ff'}}}
+                          cancelText="No"
+                        >
+                          <Button type="text" danger icon={<DeleteOutlined />} />
+                        </Popconfirm>
+                    )}
+                  </div>
                 </li>
               )}
             />
-            <Form 
-            layout="vertical"
-          >
+            <Form layout="vertical" onFinish={(values) => addComment(values.comment)}>
               <Row gutter={16}>
                 <Col span={24}>
                   <Form.Item
                     label="Add Comment"
                     name="comment"
-                    rules={[{ required: true }]}
+                    rules={[{ required: true ,message: "Please input comment"}] }
                   >
                     <Input.TextArea />
                   </Form.Item>
